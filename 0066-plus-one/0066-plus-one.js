7fc652a3261d7bee8b1580f90d6cea501926ecde
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-04-27 08:25:32 -0700

    Time: 0 ms (100%), Space: 53.7 MB (41.33%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -4,7 +4,7 @@
  */
 var plusOne = function (digits) {
     let i = digits.length - 1;
-    while (digits[i] >= 0) {
+    while (i >= 0) {
         if (digits[i] != 9) {
             digits[i]++
             return digits
