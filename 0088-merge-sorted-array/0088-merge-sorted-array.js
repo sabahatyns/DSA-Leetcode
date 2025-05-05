d6d086e69a74781841a02b46a4eb6e2f78368d15
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-05-05 10:17:32 -0700

    Time: 0 ms (100%), Space: 55 MB (51.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,20 +5,19 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;  // Pointer for the last valid element in nums1
-    let j = n - 1;  // Pointer for the last element in nums2
+function merge(nums1, m, nums2, n) {
+  let i = m - 1;         // pointer for nums1
+  let j = n - 1;         // pointer for nums2
+  let k = m + n - 1;     // pointer for placement in nums1
 
-    // Start filling nums1 from the last index
-    for (let k = m + n - 1; k >= 0; k--) {
-        if (j < 0) break;  // If nums2 is exhausted, stop
-
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
-        }
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
     }
-};
+  }
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
+}
