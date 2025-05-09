e7275f1686193193ce625f29450024d3f629e807
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-05-09 23:50:43 +0500

    Time: 34 ms (28.38%), Space: 55.9 MB (23.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,18 +1,9 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        // For each number, look for its complement
         for (let j = i + 1; j < nums.length; j++) {
-            // Check if the sum of two numbers equals the target
             if (nums[i] + nums[j] === target) {
                 return [i, j];
             }
         }
     }
-    // If no solution is found, return an empty array (shouldn't happen as per the problem statement)
-    return [];
-}
+};
