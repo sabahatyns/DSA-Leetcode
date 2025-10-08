7108d6d1a022952d99782aba2e0b76711b364022
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-10-08 14:51:59 +0500

    Time: 7 ms (47.44%), Space: 58.2 MB (10.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,19 @@
-var twoSum = function(nums, target) {
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    let map = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        map[nums[i]] = i;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map[diff] !== undefined && map[diff] !== i) {
+            return [i, map[diff]];
         }
     }
-};
+    return [];
+}
