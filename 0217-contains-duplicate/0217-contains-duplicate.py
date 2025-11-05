405842017930268a468508b2d39385dec3382f87
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-11-05 11:40:33 +0500

    Time: 24 ms (26.76%), Space: 27.3 MB (5.13%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dup = set()
+        for i in range(len(nums)):
+            if nums[i] in dup:
+                return True
+            dup.add(nums[i])
+        return False
