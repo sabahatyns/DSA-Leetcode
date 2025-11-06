df026a8e5146b86ea870bd20871f3047655da61c
sabahatyns <80611933+sabahatyns@users.noreply.github.com>
2025-11-06 17:10:50 +0500

    Time: 133 ms (24.82%), Space: 13.1 MB (61.46%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
